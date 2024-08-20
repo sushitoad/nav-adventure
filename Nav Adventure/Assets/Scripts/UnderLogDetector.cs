b316125150f92e3367f501c36b33e3c3eb8eaf3c
sushitoad <36284902+sushitoad@users.noreply.github.com>
2024-08-19 19:49:10 -0700

    hollow log bug fix

--- Nav Adventure/Assets/Scripts/UnderLogDetector.cs
+++ Nav Adventure/Assets/Scripts/UnderLogDetector.cs
@@ -10,48 +10,51 @@ public class UnderLogDetector : MonoBehaviour
     public bool exit = false;
     public int logSortingOrder = -6;
     public GameObject[] overBumpers, underBumpers;
+    MistController mistController;
 
     void Start()
     {
         Object player = FindObjectOfType<PlayerController>();
         playerSprite = player.GetComponent<SpriteRenderer>();
-        EnterExitLog(true);
+        mistController = FindObjectOfType<MistController>();
+        mistController.mistSeasonEvent += ExitLog;
+        ExitLog();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Player")
         {
-            EnterExitLog(exit);
+            if (exit) 
+            { ExitLog(); }
+            else { EntranceLog(); }
         }
     }
 
-    void EnterExitLog(bool exiting)
+    void EntranceLog()
     {
-        if (!exiting)
+        playerSprite.sortingOrder = logSortingOrder;
+        //foreach bumper in underbumper array, set active
+        foreach (GameObject bumper in underBumpers)
         {
-            playerSprite.sortingOrder = logSortingOrder;
-            //foreach bumper in underbumper array, set active
-            foreach (GameObject bumper in underBumpers)
-            {
-                bumper.gameObject.SetActive(true);
-            }
-            foreach(GameObject bumper in overBumpers)
-            {
-                bumper.gameObject.SetActive(false);
-            }
+            bumper.gameObject.SetActive(true);
         }
-        if (exiting)
+        foreach (GameObject bumper in overBumpers)
         {
-            playerSprite.sortingOrder = 0;
-            foreach (GameObject bumper in underBumpers)
-            {
-                bumper.gameObject.SetActive(false);
-            }
-            foreach (GameObject bumper in overBumpers)
-            {
-                bumper.gameObject.SetActive(true);
-            }
+            bumper.gameObject.SetActive(false);
+        }
+    }
+
+    void ExitLog()
+    {
+        playerSprite.sortingOrder = 0;
+        foreach (GameObject bumper in underBumpers)
+        {
+            bumper.gameObject.SetActive(false);
+        }
+        foreach (GameObject bumper in overBumpers)
+        {
+            bumper.gameObject.SetActive(true);
         }
     }
 }
