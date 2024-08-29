3bae6aa3ae28bb8b581101d566a13125167efb89
quillenink <36284902+sushitoad@users.noreply.github.com>
2024-08-28 20:18:18 -0700

    working on particle system

--- Nav Adventure/Assets/Scripts/MistCheckpoint.cs
+++ Nav Adventure/Assets/Scripts/MistCheckpoint.cs
@@ -7,11 +7,29 @@ public class MistCheckpoint : MonoBehaviour
     MistController mistController;
 
     public Transform mistRune;
-    public GameObject fogWall;
+    public ParticleSystem fogWall;
+    [HideInInspector] public bool activated = false;
+    float startEmissionRate, currentEmissionRate;
+    ParticleSystem.EmissionModule emission;
 
     private void Start()
     {
         mistController = FindObjectOfType<MistController>();
+        activated = false;
+        emission = fogWall.GetComponent<ParticleSystem.EmissionModule>();
+        startEmissionRate = emission.rateOverTime.constant;
+        print(startEmissionRate);
+    }
+
+    public void Update()
+    {
+        if(activated)
+        {
+            //lower emission
+        } else if(!activated)
+        {
+            //raise emission
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -22,11 +40,11 @@ public class MistCheckpoint : MonoBehaviour
             MistCheckpoint[] otherCheckpoints = FindObjectsOfType<MistCheckpoint>();
             foreach(MistCheckpoint checkpoint in otherCheckpoints)
             {
-                checkpoint.fogWall.SetActive(true);
+                checkpoint.activated = false;
             }
             //this needs to flip a bool that allows me to gradually lower ParticleSystem.emission's rate over time to zero
             //then would be good to gradually raise the emission rate of the other fogwalls
-            fogWall.SetActive(false);
+            activated = true;
         }
     }
 }
