5cdbf73516700af678f789583fecbb5714e9c777
quillenink <36284902+sushitoad@users.noreply.github.com>
2024-08-31 21:23:03 -0700

    figured out emission stuff

--- Nav Adventure/Assets/Scripts/MistCheckpoint.cs
+++ Nav Adventure/Assets/Scripts/MistCheckpoint.cs
@@ -8,32 +8,13 @@ public class MistCheckpoint : MonoBehaviour
     public float fadeSpeed = 2f;
     public Transform mistRune;
     public ParticleSystem fogWall;
-    [HideInInspector] public bool activated = false;
-    float startEmissionRate, currentEmissionRate;
+    public float emissionRate = 10f;
 
     private void Start()
     {
         mistController = FindObjectOfType<MistController>();
-        activated = false;
-        startEmissionRate = fogWall.emission.rateOverTime.constant;
-    }
-
-    public void Update()
-    {
-        currentEmissionRate = fogWall.emission.rateOverTime;
-        if(activated && currentEmissionRate > 0f)
-        {
-            //lower emission
-            float reduction = Time.deltaTime * fadeSpeed;
-            fogWall.emission.rateOverTime -= reduction;
-            if(currentEmissionRate < 0f)
-            {
-                fogWall.emission.rateOverTime = 0f;
-            }
-        } else if(!activated)
-        {
-            //raise emission
-        }
+        var emission = fogWall.emission;
+        emission.rateOverTime = emissionRate;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -44,11 +25,11 @@ public class MistCheckpoint : MonoBehaviour
             MistCheckpoint[] otherCheckpoints = FindObjectsOfType<MistCheckpoint>();
             foreach(MistCheckpoint checkpoint in otherCheckpoints)
             {
-                checkpoint.activated = false;
+                var em = checkpoint.fogWall.emission;
+                em.rateOverTime = emissionRate;
             }
-            //this needs to flip a bool that allows me to gradually lower ParticleSystem.emission's rate over time to zero
-            //then would be good to gradually raise the emission rate of the other fogwalls
-            activated = true;
+            var emission = fogWall.emission;
+            emission.rateOverTime = 0f;
         }
     }
 }
